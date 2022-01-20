d0bdf939dce1410ef31d284827ee14e38efa5833
ludo <85556848+ludolfaugust@users.noreply.github.com>
2022-01-20 07:47:36 +0100

    add cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 //import Slider from "./components/Slider";
-import PhoneIcon from "@mui/icons-material/Phone";
+
+import Cards from "./components/Products/Cards.js/Cards";
+
+import Grid from "@mui/material/Grid";
 
 function App() {
   return (
     <div className="App">
       <h1>Coding Exercises with material-ui and Bootstrap</h1>
       <Navbar />
-      <PhoneIcon />
+
       {/* <Slider /> */}
+      <Cards />
     </div>
   );
 }
